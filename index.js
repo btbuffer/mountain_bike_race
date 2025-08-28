777990f25e529674643fcf470510c1bfc1a414d2
btbuffer <jimohsikiru20@gmail.com>
2025-08-28 14:00:16 +0100

    Add index.js entry file

---
+++ index.js
@@ -0,0 +1,22 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const routes = require("./routes/index.route");
+
+// She boards a car with plate number: APP 907 CM
+dotenv.config({ path: "./.env", quiet: true });
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(routes);
+app.listen(PORT, () => {
+  console.log(`MountRacer listens on port ${PORT}`);
+});
+
+mongoose
+  .connect(process.env.DBURI)
+  .then(() => console.log("MongoDB connection established!"))
+  .catch((err) => console.log("MongoDB connectionError: ", err.message));
