b20c8acadb5911d9cc3ccde9b1cc04e8143a6aad
btbuffer <jimohsikiru20@gmail.com>
2025-08-29 07:49:37 +0100

    Modify the race route path

--- routes/race.route.js
+++ routes/race.route.js
@@ -3,6 +3,6 @@ const { Router } = require("express");
 const { createRace } = require("../controller/race.controller");
 
 const router = Router();
-router.post("/api/race/register", createRace);
+router.post("/api/race", createRace);
 
 module.exports = router;
