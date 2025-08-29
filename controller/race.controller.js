c2a7e7025859f9f136c06cf4fd3846f3580292ee
btbuffer <jimohsikiru20@gmail.com>
2025-08-29 08:57:47 +0100

    Update race logic with route handler for weather information

--- controller/race.controller.js
+++ controller/race.controller.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 const { Race } = require("../models/index.model");
 
 const createRace = async (request, response) => {
@@ -9,4 +11,20 @@ const createRace = async (request, response) => {
   return response.status(201).send(race);
 };
 
-module.exports = { createRace };
+const getRaceWeatherInfo = async (request, response) => {
+  const {
+    raceDetails: { race },
+  } = request;
+  const apiKey = process.env.OPENWEATHER_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${race.location}&APPID=${apiKey}&units=metric`;
+  const res = await axios.get(url);
+  const weather = res.data;
+  return response.send({
+    location: race.location,
+    temperature: weather.main.temp,
+    conditions: weather.weather[0].description,
+    humidity: weather.main.humidity,
+  });
+};
+
+module.exports = { createRace, getRaceWeatherInfo };
