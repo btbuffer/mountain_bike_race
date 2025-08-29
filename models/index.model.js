332bd1d7fc5dfdf39b7694a4767279ecc002139d
btbuffer <jimohsikiru20@gmail.com>
2025-08-29 03:32:10 +0100

    Add pre hook for race schema

--- models/index.model.js
+++ models/index.model.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 const RiderSchema = require("./rider.model");
+const RaceSchema = require("./race.model");
+
 const { isAthleteExist, generateAthleteId } = require("../utils/rider.util");
 
 RiderSchema.pre("validate", async function () {
@@ -18,6 +20,21 @@ RiderSchema.pre("validate", async function () {
   this.athleteId = athleteId;
 });
 
-const Rider = mongoose.model("Rider", RiderSchema);
+RaceSchema.pre("validate", async function () {
+  // let eventDate = new Date(this.date);
+  // const dateComponents = eventDate
+  //   .toLocaleString("en-US", { timeZone: "Africa/Lagos" })
+  //   .split(",");
+  // console.log("Date Component: ", dateComponents);
+  // const [date, time] = dateComponents;
+  // const [hr] = time.trim().split(" ");
+  // let isoDate = `${date.trim().replace(/\//g, "-")}`;
+  // isoDate = isoDate.split("-").reverse().join("-") + "T" + hr.trim();
+  // console.log("Date: ", isoDate);
+  // console.log("Date: ", new Date(isoDate));
+});
 
-module.exports = Rider;
+const Rider = mongoose.model("Rider", RiderSchema);
+const Race = mongoose.model("Race", RaceSchema);
+
+module.exports = { Rider, Race };
