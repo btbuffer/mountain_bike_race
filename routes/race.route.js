d9279138608e9edeae549b7ca95ffc3d338aaea7
btbuffer <jimohsikiru20@gmail.com>
2025-08-29 09:00:44 +0100

    Update race route for weather endpoint

--- routes/race.route.js
+++ routes/race.route.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 
-const { createRace } = require("../controller/race.controller");
+const {
+  createRace,
+  getRaceWeatherInfo,
+} = require("../controller/race.controller");
+const validateRaceId = require("../utils/verify.raceId");
 
 const router = Router();
 router.post("/api/race", createRace);
+router.get("/api/race/:raceId/weather", validateRaceId, getRaceWeatherInfo);
 
 module.exports = router;
