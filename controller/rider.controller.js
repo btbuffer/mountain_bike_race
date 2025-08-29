aa84401fdf2727f3da6d91c4cdb3ccadb9e87b46
btbuffer <jimohsikiru20@gmail.com>
2025-08-29 03:33:35 +0100

    Add route handler for data mgt

--- controller/rider.controller.js
+++ controller/rider.controller.js
@@ -1,4 +1,5 @@
-const Rider = require("../models/index.model");
+const { Rider } = require("../models/index.model");
+const Result = require("../models/result.model");
 
 const createRider = async (request, response) => {
   const { body } = request;
@@ -9,4 +10,60 @@ const createRider = async (request, response) => {
   return response.status(201).send(rider);
 };
 
-module.exports = { createRider };
+const getFastestRiders = async (request, response) => {
+  const { raceId } = request;
+  const raceResults = await Result.find({
+    race: raceId,
+    finishTime: { $ne: null },
+  })
+    .sort({ finishTime: "asc" })
+    .limit(3)
+    .populate("rider", "firstname lastname -_id");
+
+  const riders = raceResults.map((entry) => ({
+    name: `${entry.rider.firstname} ${entry.rider.lastname}`,
+    timeTaken: entry.finishTime,
+  }));
+
+  return response.status(200).send(riders);
+};
+
+const getNonFinishers = async (request, response) => {
+  const { raceId } = request;
+
+  const incompleteRaceRiders = await Result.find({
+    race: raceId,
+    finishTime: null,
+  }).populate("rider", "firstname lastname -_id");
+
+  const nonFinishers = incompleteRaceRiders.map((entry) => ({
+    name: `${entry.rider.firstname} ${entry.rider.lastname}`,
+  }));
+
+  return response.status(200).send(nonFinishers);
+};
+
+const getNotParticipants = async (request, response) => {
+  const { raceId } = request;
+
+  const participants = await Result.find({ race: raceId }).distinct("rider");
+  const nonparticipants = await Rider.find(
+    {
+      _id: { $nin: participants },
+    },
+    "firstname lastname -_id"
+  );
+
+  return response.send(
+    nonparticipants.map((nonparticipant) => ({
+      name: `${nonparticipant.firstname} ${nonparticipant.lastname}`,
+    }))
+  );
+};
+
+module.exports = {
+  createRider,
+  getFastestRiders,
+  getNonFinishers,
+  getNotParticipants,
+};
